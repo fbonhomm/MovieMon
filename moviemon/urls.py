2f0ad933192c53182fc2e5b2c5769f7246e5be22
Baptiste BICHERON <bbichero@e1r10p1.42.fr>
2018-10-06 14:57:10 +0200

    add all route for front

--- moviemon/urls.py
+++ moviemon/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
 from django.conf.urls import url
-
-from moviemon import views
+from . import views
 
 urlpatterns = [
-  url(r'^$', views.index),
+    url(r'^TitleScreen/', views.TitleScreen, name='TitleScreen'),
+    url(r'^WorldMap/', views.WorldMap, name='WordlMap'),
+    url(r'^Battle/', views.Battle, name='Battle'),
+    url(r'^Moviedex/', views.Moviedex, name='Moviedex'),
+    url(r'^Detail/', views.Detail, name='Detail'),
+    url(r'^Option/', views.Option, name='Option'),
+    url(r'^Save/', views.Save, name='Save'),
+    url(r'^Load/', views.Load, name='Load')
 ]
