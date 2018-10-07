6ebfda6bd7efa195774e4770b2c390a5e4f2ded8
Baptiste BICHERON <bbichero@e1r10p1.42.fr>
2018-10-07 03:21:38 +0200

    add urls match for title and world route

--- moviemon/urls.py
+++ moviemon/urls.py
@@ -4,10 +4,10 @@ from . import views
 urlpatterns = [
     url(r'^TitleScreen/', views.TitleScreen, name='TitleScreen'),
     url(r'^WorldMap/', views.WorldMap, name='WordlMap'),
-    url(r'^Battle/', views.Battle, name='Battle'),
-    url(r'^Moviedex/', views.Moviedex, name='Moviedex'),
-    url(r'^Detail/', views.Detail, name='Detail'),
-    url(r'^Option/', views.Option, name='Option'),
-    url(r'^Save/', views.Save, name='Save'),
-    url(r'^Load/', views.Load, name='Load')
+#    url(r'^Battle/', views.Battle, name='Battle'),
+#    url(r'^Moviedex/', views.Moviedex, name='Moviedex'),
+#    url(r'^Detail/', views.Detail, name='Detail'),
+#    url(r'^Option/', views.Option, name='Option'),
+#    url(r'^Save/', views.Save, name='Save'),
+#    url(r'^Load/', views.Load, name='Load')
 ]
